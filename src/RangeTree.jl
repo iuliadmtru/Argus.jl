7a717e18240fe7dbd697c609144a860b435ce5c5
Iulia Dumitru <iuliadmtru@gmail.com>
2025-03-13 19:30:06 +0200

    Move RangeTree to separate package

--- src/RangeTree.jl
+++
@@ -1,9 +0,0 @@
-using AbstractTrees
-
-struct RangeTree <: AbstractNode{T}
-    interval::UnitRange # Should keep `low` and `high` instead?
-    children::AbstractVector{RangeTree}
-    max::Int
-    value::T
-end
-children(tree::RangeTree) = tree.children
