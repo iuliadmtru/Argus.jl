a8a8d4111d5aa8eabfae63a57b3d64ff9f1617f9
Iulia Dumitru <iuliadmtru@gmail.com>
2025-03-31 17:45:54 +0300

    Add tests for `Pattern`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using Argus
 using JuliaSyntax
-using JuliaSyntax: source_location
 
+include("syntax_template_node.jl")
 include("syntax_template.jl")
+include("syntax_pattern.jl")
---
+++ test/syntax_pattern.jl
@@ -0,0 +1,19 @@
+@testset "AbstractSyntaxPattern" begin
+
+    @testset "Pattern" begin
+        src = """
+        function f(a, b)
+            y = a + b
+            return y
+        end
+        """
+        pattern = Pattern(src)
+        @test !contains_placeholders(pattern.templ)
+        syntax_node = JuliaSyntax.parsestmt(JuliaSyntax.SyntaxNode, src)
+        matches = pattern_match!(pattern, syntax_node)
+        @test length(matches) == 1
+        m = matches[1]
+        @test head(m.ast) == head(syntax_node)
+    end
+
+end
