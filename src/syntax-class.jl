1d77371f0a3a910012b007949c4bbb6acae0fdd5
Iulia Dumitru <iuliadmtru@gmail.com>
2025-05-09 22:56:22 +0300

    Start implementing the matching mechanism

--- src/syntax-class.jl
+++ src/syntax-class.jl
@@ -8,13 +8,13 @@ specifies a syntactic "shape" and provides a description for that shape. For exa
 a syntax class corresponding to a binary function call could be defined as such:
 ```
 binary_funcall = @syntax_class "binary function call" quote
-    ~var(_(_, _))
+    _(_, _)
 end
 ```
 """
 struct SyntaxClass
     description::String
-    alternatives::Vector{Pattern}
+    pattern_alternatives::Vector{Pattern}
 end
 
 # TODO: Support for fail conditions.
@@ -25,4 +25,28 @@ macro syntax_class(description, patterns)
         Pattern.(args[1].args)                              :
         Pattern.(args)
     return :( SyntaxClass($description, $alternatives) )
+end
+
+"""
+Global registry containing all registered syntax classes. When registering a syntax class,
+it is not necessary for it to be defined. The registry can be checked for consistency
+before matching using [`syntax_class_registry_check`](@ref).
+"""
+SYNTAX_CLASS_REGISTRY = Dict{Symbol, Union{Nothing, SyntaxClass}}()
+
+function register_syntax_class!(name::Symbol)
+    # TODO: Interactive overwrite?
+    SYNTAX_CLASS_REGISTRY[name] = nothing
+end
+function register_syntax_class!(name::Symbol, syntax_class::SyntaxClass)
+    # TODO: Interactive overwrite?
+    SYNTAX_CLASS_REGISTRY[name] = syntax_class
+end
+
+function syntax_class_registry_check()
+    for (syntax_class_name, syntax_class) in SYNTAX_CLASS_REGISTRY
+        isnothing(syntax_class) &&
+            error("Syntax class $syntax_class_name is referenced but undefined")
+        # TODO: Check for syntax classes that reference undefined syntax classes.
+    end
 end
---
+++ src/syntax-match.jl
@@ -0,0 +1,116 @@
+struct MatchFail
+    message::String
+end
+MatchFail() = MatchFail("no match")
+
+function check(fail_conditions::Vector{Function},
+               bindings::BindingSet)::Union{MatchFail, BindingSet}
+    # TODO.
+    return bindings
+end
+check(::Vector{Function}, failure::MatchFail)::MatchFail = failure
+
+function syntax_match(pattern::Pattern,
+                      src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    bindings = syntax_match(pattern.ast, src)
+    return check(pattern.fail_conditions, bindings)
+end
+function syntax_match(syntax_class::SyntaxClass,
+                      src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    for pattern in syntax_class.pattern_alternatives
+        match_result = syntax_match(pattern, src)
+        # Return the first successful match.
+        isa(match_result, BindingSet) && return match_result
+        # TODO: Track the failures and return the most relevant one.
+    end
+    # If neither of the pattern alternatives matched, `src` does not match the syntax_class.
+    return MatchFail()
+end
+function syntax_match(pattern_node::SyntaxPatternNode,
+                      src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    # Special syntax.
+    is_pattern_form(pattern_node) && return syntax_match_pattern_form(pattern_node, src)
+    # Regular syntax.
+    success = BindingSet()
+    if head(pattern_node) != head(src)
+        # A `[=]` node in the pattern can match a short form function definition.
+        if kind(pattern_node) === K"=" && kind(src) === K"function"
+            src_flags = JuliaSyntax.flags(src)
+            src_flags === JuliaSyntax.SHORT_FORM_FUNCTION_FLAG || return MatchFail()
+        else
+            return MatchFail()
+        end
+    end
+    pattern_node.data.val != src.data.val && return MatchFail()
+    xor(is_leaf(pattern_node), is_leaf(src)) && return MatchFail()
+    # Recurse on children if there are any.
+    is_leaf(src) && return success
+    length(children(pattern_node)) != length(children(src)) && return MatchFail()
+    zipped_children = zip(children(pattern_node), children(src))
+    for c_pair in zipped_children
+        match_result = syntax_match(c_pair[1], c_pair[2])
+        if isa(match_result, MatchFail)
+            return match_result
+        else
+            union!(success, match_result)
+        end
+    end
+    return success
+end
+
+# --------------------------------------------
+# Pattern form matching.
+
+"""
+Try to match a pattern form node. Dispatch the matching to the specific pattern form
+match function.
+"""
+function syntax_match_pattern_form(pattern_node::SyntaxPatternNode,
+                                   src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    isa(pattern_node.data, VarSyntaxData) && return syntax_match_var(pattern_node, src)
+    isa(pattern_node.data, OrSyntaxData) && return syntax_match_or(pattern_node, src)
+    return MatchFail("unknown pattern form")
+end
+
+"""
+Try to match a `var` pattern form. If there's a match, bind the pattern variable.
+"""
+function syntax_match_var(var_node::SyntaxPatternNode,
+                          src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    @info "match var" var_node src
+    pattern_var_name = var_node.data.id
+    syntax_class_name = var_node.data.syntax_class_name
+    syntax_class =
+        try
+            SYNTAX_CLASS_REGISTRY[syntax_class_name]
+        catch e
+            if isa(e, KeyError)
+                return MatchFail("unregistered syntax class $syntax_class_name")
+            else
+                rethrow(e)
+            end
+        end
+    # Try to match the pattern syntax class to the AST.
+    match_result = syntax_match(syntax_class, src)
+    isa(match_result, MatchFail) && return match_result
+    # If there's a match, bind the pattern variable and return the binding as a
+    # `BindingSet`.
+    binding = Binding(pattern_var_name, src, match_result)
+    # TODO: Check for already existing binding. The new binding must be "compatible" with
+    #       the old one.
+    return BindingSet(pattern_var_name => binding)
+end
+
+"""
+Try to match an `or` pattern form. Return the match for the first pattern alternative.
+Fail if no alternative matches.
+"""
+function syntax_match_or(or_node::SyntaxPatternNode,
+                         src::JuliaSyntax.SyntaxNode)::Union{MatchFail, BindingSet}
+    for p in children(or_node)
+        match_result = syntax_match(p, src)
+        isa(match_result, BindingSet) && return match_result
+    end
+    # TODO: Return the most specific error.
+    return MatchFail("no alternative match")
+end
