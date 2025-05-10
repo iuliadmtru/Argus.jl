5fd8b9ed7eb4eb2b5185ffdc1482525cc3bba420
Iulia Dumitru <iuliadmtru@gmail.com>
2025-05-10 19:30:57 +0300

    Add rules

--- src/Argus.jl
+++ src/Argus.jl
@@ -1,18 +1,27 @@
 module Argus
 
+using JuliaSyntax: JuliaSyntax, head, kind, @K_str,
+    children, is_leaf, is_identifier, source_location,
+    @isexpr
+
+# Part 1: Pattern matching mechanism.
+
 export SyntaxPatternNode, Pattern, SyntaxClass, @syntax_class,
     Binding, BindingSet, MatchFail
 export register_syntax_class!, syntax_class_registry_check
 export syntax_match
-
-using JuliaSyntax: JuliaSyntax, head, kind, @K_str,
-    children, is_leaf, is_identifier, source_location
 
 include("syntax-pattern-node.jl")
 include("pattern.jl")
 include("syntax-class.jl")
 include("bindings.jl")
 include("syntax-match.jl")
+
+# Part 2: Rule writing mechanism.
+
+export Rule, @rule
+
+include("rules.jl")
 
 function __init__()
     _register_kinds()
