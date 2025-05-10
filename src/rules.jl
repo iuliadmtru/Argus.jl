44bd62638431e1edd5cd490182884d6f3d37b790
Iulia Dumitru <iuliadmtru@gmail.com>
2025-05-10 20:23:49 +0300

    Add rule match; fix rules

--- src/rules.jl
+++ src/rules.jl
@@ -1,49 +1,82 @@
+using MacroTools: MacroTools, striplines
+
 struct Rule
     name::String
     description::String
     pattern::Pattern
 end
 
-Rule(name::String, description::String, pattern::Expr) =
+Rule(name::String, description::String, pattern) =
     Rule(name, description, Pattern(pattern))
 
 macro rule(name, ex)
     # Remove line number nodes.
     rule = MacroTools.striplines(ex)
     # Check the rule syntax.
-    @isexpr(rule, :block) || error("Invalid @rule syntax: $rule")
+    @isexpr(rule, :quote, 1) || error("Invalid rule syntax:\n$rule")
+    rule = rule.args[1]
     length(rule.args) == 2 ||
-        error("Invalid @rule syntax: $rule\n",
+        error("Invalid rule syntax: $rule\n",
               "Expected 2 arguments, got $(length(rule.args))")
     # Get the first argument and see whether it's a description or a pattern argument.
     arg1 = rule.args[1]
-    @isexpr(arg1, :(=), 2) || error("Invalid @rule argument syntax: $arg1")
-    arg1_type = arg1.args[1]
-    if arg1_type === :description
+    @isexpr(arg1, :(=), 2) || error("Invalid rule argument syntax:\n$arg1")
+    arg1_name = arg1.args[1]
+    if arg1_name === :description
         description = arg1.args[2]
         isa(description, String) ||
-            error("Invalid description type $(typeof(description)): $description")
+            error("Invalid description type in $description\n",
+                  "Expected String, got $(typeof(description))")
         # Get the second argument, which should be the pattern.
-        pattern_expr = rule.args[2]
-        @isexpr(pattern_expr, :(=), 2) ||
-            error("Invalid @rule argument syntax: $pattern_expr")
-        pattern_expr.args[1] === :pattern ||
-            error("Invalid @rule argument name: $(pattern_expr.args[1])")
-        pattern = pattern_expr.args[2]
-        # Don't include the wrapping quote in the pattern.
-        if isa(pattern, Expr) && pattern.head === :quote
-            pattern = pattern.args[1]
-        end
-    elseif arg1_type === :pattern
-        pattern = pattern_expr.args[2]
-        # Don't include the wrapping quote in the pattern.
-        if isa(pattern, Expr) && pattern.head === :quote
-            pattern = pattern.args[1]
-        end
+        pattern_node = rule.args[2]
+        @isexpr(pattern_node, :(=), 2) ||
+            error("Invalid rule argument syntax:\n$pattern_node")
+        pattern_node.args[1] === :description &&
+            error("Duplicate rule argument: description")
+        pattern_node.args[1] === :pattern ||
+            error("Invalid rule argument: $(pattern_node.args[1])")
+        pattern = pattern_node.args[2]
+    elseif arg1_name === :pattern
+        pattern = arg1.args[2]
         # Get the second argument, which should be the description.
+        description_node = rule.args[2]
+        @isexpr(description_node, :(=), 2) ||
+            error("Invalid rule argument syntax:\n$description_node")
+        description_node.args[1] === :pattern &&
+            error("Duplicate rule argument: pattern")
+        description_node.args[1] === :description ||
+            error("Invalid rule argument: $(description_node.args[1])")
+        description = description_node.args[2]
+        isa(description, String) ||
+            error("Invalid description type in $description\n",
+                  "Expected String, got $(typeof(description))")
     else
-        error("Invalid @rule argument: $arg1_type")
+        error("Invalid rule argument: $arg1_name")
     end
 
     return :( Rule($name, $description, $pattern) )
+end
+
+struct RuleMatchResult
+    failures::Vector{MatchFail}
+    matches::Vector{BindingSet}
+end
+RuleMatchResult() = RuleMatchResult([], [])
+
+Base.push!(rule_match_result::RuleMatchResult, res) =
+    isa(res, MatchFail)                    ?
+    push!(rule_match_result.failures, res) :
+    push!(rule_match_result.matches, res)
+
+function rule_match(rule::Rule, src::JuliaSyntax.SyntaxNode; only_matches=true)
+    rule_result = RuleMatchResult()
+    match_result = syntax_match(rule.pattern, src)
+    push!(rule_result, match_result)
+    # Recurse on children, if any. Collect all match results.
+    is_leaf(src) && return only_matches ? rule_result.matches : rule_result
+    for c in children(src)
+        match_result = syntax_match(rule.pattern, src)
+        push!(rule_result, match_result)
+    end
+    return only_matches ? rule_result.matches : rule_result
 end
