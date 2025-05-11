1e366770ae3c5cb4c998ce9495f0b0d98f47b409
Iulia Dumitru <iuliadmtru@gmail.com>
2025-05-12 00:38:22 +0300

    Add back rule groups

--- src/rules.jl
+++ src/rules.jl
@@ -1,5 +1,8 @@
 using MacroTools: MacroTools, striplines
 
+# ------------------------------------------------------------------------------------------
+# Rules.
+
 struct Rule
     name::String
     description::String
@@ -10,10 +13,13 @@ Rule(name::String, description::String, pattern) =
     Rule(name, description, Pattern(pattern))
 
 macro rule(name, ex)
-    # Remove line number nodes.
+    # Remove line number nodes and unescape.
     rule = MacroTools.striplines(ex)
+    rule = Meta.unescape(rule)
     # Check the rule syntax.
-    @isexpr(rule, :quote, 1) || error("Invalid rule syntax:\n$rule")
+    @isexpr(rule, :quote, 1) ||
+        error("Invalid rule syntax:\n```\n$rule\n```\n",
+              "Rules should be defined inside `quote ... end`.")
     rule = rule.args[1]
     length(rule.args) == 2 ||
         error("Invalid rule syntax: $rule\n",
@@ -56,6 +62,112 @@ macro rule(name, ex)
 
     return :( Rule($name, $description, $pattern) )
 end
+
+# Display.
+
+function Base.show(io::IO, rule::Rule)
+    println(io, rule.name, ": ", rstrip(rule.description))
+    show(io, MIME("text/plain"), rule.pattern)
+end
+
+# ------------------------------------------------------------------------------------------
+# Rule groups.
+
+const DEFAULT_RULE_GROUP_NAME = "default"
+
+struct RuleGroup <: AbstractDict{String, Rule}
+    name::String
+    rules::Dict{String, Rule}
+
+    RuleGroup() = new(DEFAULT_RULE_GROUP_NAME, Dict{String, Rule}())
+    RuleGroup(name::String) = new(name, Dict{String, Rule}())
+    RuleGroup(kvs) = new(DEFAULT_RULE_GROUP_NAME, Dict{String, Rule}(kvs))
+    RuleGroup(name::String, kvs) = new(name, Dict{String, Rule}(kvs))
+end
+
+# Dict interface.
+
+Base.isempty(rg::RuleGroup) = isempty(rg.rules)
+Base.empty!(rg::RuleGroup) = empty!(rg.rules)
+Base.length(rg::RuleGroup) = length(rg.rules)
+
+Base.iterate(rg::RuleGroup) = iterate(rg.rules)
+Base.iterate(rg::RuleGroup, i::Int) = iterate(rg.rules, i)
+Base.setindex!(rg::RuleGroup, v, k...) = setindex!(rg.rules, v, k...)
+
+Base.haskey(rg::RuleGroup, k) = haskey(rg.rules, k)
+Base.get(rg::RuleGroup, k, d) = get(rg.rules, k, d)
+Base.get(f::Union{Function, Type}, rg::RuleGroup, k) = get(f, rg.rules, k)
+Base.get!(rg::RuleGroup, k, d) = get!(rg.rules, k, d)
+Base.get!(f::Union{Function, Type}, rg::RuleGroup, k) = get!(f, rg.rules, k)
+Base.getkey(rg::RuleGroup, k, d) = getkey(rg.rules, k, d)
+Base.delete!(rg::RuleGroup, k) = delete!(rg.rules, k)
+Base.pop!(rg::RuleGroup, k) = pop!(rg.rules, k)
+Base.pop!(rg::RuleGroup, k, d) = pop!(rg.rules, k, d)
+Base.keys(rg::RuleGroup) = keys(rg.rules)
+Base.values(rg::RuleGroup) = values(rg.rules)
+Base.pairs(rg::RuleGroup) = pairs(rg.rules)
+Base.merge(rg::RuleGroup, others::RuleGroup...) =
+    RuleGroup(merge(rg.rules, others...))
+Base.mergewith(c, rg::RuleGroup, others::RuleGroup...) =
+    RuleGroup(mergewith(c, rg.rules, others...))
+Base.merge!(rg::RuleGroup, others::RuleGroup...) =
+    RuleGroup(merge!(rg.rules, others...))
+Base.mergewith!(c, rg::RuleGroup, others::RuleGroup...) =
+    RuleGroup(mergewith!(c, rg.rules, others...))
+Base.keytype(rg::RuleGroup) = keytype(rg.rules)
+Base.valtype(rg::RuleGroup) = valtype(rg.rules)
+
+# Display.
+
+function Base.summary(io::IO, rg::RuleGroup)
+    Base.showarg(io, rg, true)
+    n = length(rg)
+    print(io, "(\"", rg.name, "\") with ", n, (n==1 ? " entry" : " entries"))
+end
+
+Base.show(io::IO, rg::RuleGroup) =
+    isempty(rg)                               ?
+    print(io, "RuleGroup(\"", rg.name, "\")") :
+    invoke(show, Tuple{IOBuffer, AbstractDict}, io, rg)
+
+# --------------------------------------------
+# Rule definition in groups.
+
+function define_rule_in_group(group::RuleGroup, rule_name::String, rule::Rule)
+    # rule = @rule(rule_name, rule_expr)
+    register_rule!(group, rule)
+
+    return rule
+end
+
+# # TODO: Make internal.
+# function define_rule_in_group(group::RuleGroup, rule_name::String, rule_str::String)
+#     rule = Meta.parse(rule_str)
+#     return define_rule_in_group(group, rule_name, rule)
+# end
+
+# # TODO: Find a way to make this nicer...
+# handle_define_rule_in_group(group, rule_name, rule_str) =
+#     esc( :($define_rule_in_group($group, $rule_name, $rule_str)) )
+
+macro define_rule_in_group(group, rule_name, rule_expr)
+    # rule_str = string(rule)
+    # handle_define_rule_in_group(group, rule_name, rule_str)
+    rule = :( @rule($(esc(rule_name)), $(esc(rule_expr))) )
+    return :( define_rule_in_group($(esc(group)), $(esc(rule_name)), $rule) )
+end
+
+# --------------------------------------------
+# Utils.
+
+function register_rule!(group::RuleGroup, rule::Rule)
+    # TODO: Add interactive "overwrite?".
+    group[rule.name] = rule
+end
+
+# ------------------------------------------------------------------------------------------
+# Rule matching.
 
 struct RuleMatchResult
     failures::Vector{MatchFail}
