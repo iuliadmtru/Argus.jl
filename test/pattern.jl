4f60e8405dd19b3cf8848490959d797aa170d60e
Iulia Dumitru <iuliadmtru@gmail.com>
2025-06-12 18:25:10 +0300

    Binding test file

--- test/pattern.jl
+++ test/pattern.jl
@@ -196,64 +196,6 @@
             end
         end
 
-        # TODO: Move these to `test/bindings.jl`.
-        # Bindings fields access.
-        let
-            pattern = @pattern begin
-                {x}
-                @fail x.name == "a" "is a"
-            end
-            @test isa(syntax_match(pattern, parsestmt(SyntaxNode, "b")), BindingSet)
-            @test syntax_match(pattern, parsestmt(SyntaxNode, "a")) == MatchFail("is a")
-            field_err_literal = syntax_match(pattern, parsestmt(SyntaxNode, "2"))
-            @test isa(field_err_literal, MatchFail)
-            @test field_err_literal.message ==
-                """
-                BindingFieldError: binding `x` has no field `name` because the bound expression is not an identifier.
-                Available fields: `value`
-
-                The following fields are internal, avoid using them in patterns: `bname`, `src`, `bindings`
-                """
-            field_err_expr = syntax_match(pattern, parsestmt(SyntaxNode, "x = y"))
-            @test isa(field_err_literal, MatchFail)
-            @test startswith(field_err_expr.message,
-                             """
-                             BindingFieldError: binding `x` has no field `name` because the bound expression is not an identifier.
-                             Available fields: none
-                             """)
-        end
-        let
-            pattern = @pattern begin
-                {x:::assign}
-                @fail x.rhs.value == 2 "rhs is two"
-            end
-            @test isa(syntax_match(pattern, parsestmt(SyntaxNode, "a = 3")), BindingSet)
-            @test syntax_match(pattern, parsestmt(SyntaxNode, "3")) ==
-                MatchFail("not an assignment")
-            @test syntax_match(pattern, parsestmt(SyntaxNode, "x = 2")) ==
-                MatchFail("rhs is two")
-            field_err = syntax_match(pattern, parsestmt(SyntaxNode, "x = y"))
-            @test isa(field_err, MatchFail)
-            @test startswith(field_err.message,
-                             """
-                             BindingFieldError: binding `rhs` has no field `value` because the bound expression is not a literal.
-                             Available fields: `name`
-                             """)
-        end
-        let
-            pattern = @pattern begin
-                {x:::identifier}
-                @fail x.abc.name == "abc" "is abc"
-            end
-            field_err = syntax_match(pattern, parsestmt(SyntaxNode, "a"))
-            @test isa(field_err, MatchFail)
-            @test startswith(field_err.message,
-                             """
-                             BindingFieldError: binding `x` has no field `abc` because `abc` is not a sub-binding of `x`.
-                             Available fields: `_id`, `name`
-                             """)
-        end
-
         @testset "Pattern matching" begin
             let
                 binary_funcall_pattern = @pattern ({f:::identifier})({arg1}, {_})
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Argus
 using JuliaSyntax
 using JuliaSyntax: source_location, kind, @K_str, is_leaf, ParseError
 
+include("bindings.jl")
 include("pattern.jl")
 include("syntax-class.jl")
 include("rules.jl")
