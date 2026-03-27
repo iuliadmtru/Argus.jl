f059b83f1c08f69cb2555e4c7cad89d84cef9ddf
Iulia Dumitru <iuliadmtru@gmail.com>
2026-03-27 13:21:33 +0100

    Fix: search for parens at the correct bytes

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,11 @@
 function inside_parens(src::JS.SyntaxNode)
     source_file = JS.sourcefile(src)
     source_byte_range = JS.byte_range(src)
-    view(source_file, source_byte_range .- 1) == "(" || return false
-    view(source_file, source_byte_range .+ 1) == ")" || return false
+    prev_byte = source_byte_range[1] - 1
+    next_byte = source_byte_range[end] + 1
+    # Check that we have an opening parenthesis at `prev_byte` and a closing one at
+    # `next_byte`.
+    view(source_file, prev_byte:prev_byte) == "(" || return false
+    view(source_file, next_byte:next_byte) == ")" || return false
     return true
 end
